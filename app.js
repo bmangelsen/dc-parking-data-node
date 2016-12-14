7ee146babfce6750df54abbdf66b261106ca7b99
Ben Mangelsen <bmangelsen@gmail.com>
2016-12-14 18:14:52 -0500

    Removed unnecessary log

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ var countRows = require('./row_count');
 var countTicketTypes = require('./ticket_types');
 var mostCommonType = require('./most_common_type');
 
-console.log("How many parking tickets were issued?");
-
-
 require('./parser')('./simple_data/parking_feb_2016.csv', function(err, theData) {
   var parsedData = theData;
   countRows(parsedData, function(err, countedRowData) {
